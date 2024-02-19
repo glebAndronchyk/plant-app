eaed06044e067c3eb73bddab4263d066ec7fd6ae
Gleb Andronchyk <gandronchyk@ek-mb-086.local>
2024-02-19 19:32:16 +0200

    fix: home scroll view

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,16 @@
-import {View} from 'react-native';
+import {ScrollView} from 'react-native';
 import {WeatherWidget} from '@components/WeatherWidget';
 import {containerStyles} from '@theme/containers.ts';
 import {SmallGardenView} from '@components/Garden/SmallGardenView';
 
 export const HomeScreen = () => {
   return (
-    <View style={[containerStyles().marginContainer, {gap: 24}]}>
+    <ScrollView
+      contentContainerStyle={[containerStyles().marginContainer, {gap: 24}]}>
       <WeatherWidget />
       <SmallGardenView />
       {/*<ShareCareLog />*/}
       {/*<RecommendedChats />*/}
-    </View>
+    </ScrollView>
   );
 };
